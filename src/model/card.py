13593789569a691dbe2391cb1e5cfdfa32e49bff
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-04-24 22:54:25 +0200

    Add a Card class.

---
+++ src/model/card.py
@@ -0,0 +1,21 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import dataclasses
+
+from model.card_value import CardValue
+from model.suit import Suit
+
+
+@dataclasses.dataclass(order=False, frozen=True)
+class Card:
+  card_value: CardValue
+  suit: Suit
+
+  def __post_init__(self):
+    if self.card_value is None or self.suit is None:
+      raise ValueError("card_value and suit cannot be None")
+
+  def __str__(self):
+    return "%s%s" % (self.card_value, self.suit)
---
+++ src/model/card_test.py
@@ -0,0 +1,45 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import dataclasses
+import unittest
+
+from model.card import Card
+from model.card_value import CardValue
+from model.suit import Suit
+
+
+class CardTest(unittest.TestCase):
+  @staticmethod
+  def test_create_and_print_all_cards():
+    for card_value in CardValue:
+      for suit in Suit:
+        card = Card(card_value, suit)
+        print(repr(card), card)
+
+  # noinspection PyTypeChecker
+  def test_card_value_and_suit_cannot_be_none(self):
+    with self.assertRaisesRegex(ValueError,
+                                "card_value and suit cannot be None"):
+      print(Card(CardValue.ACE, None))
+    with self.assertRaisesRegex(ValueError,
+                                "card_value and suit cannot be None"):
+      print(Card(None, Suit.DIAMONDS))
+    with self.assertRaisesRegex(ValueError,
+                                "card_value and suit cannot be None"):
+      print(Card(None, None))
+
+  def test_immutable(self):
+    card = Card(CardValue.ACE, Suit.DIAMONDS)
+    with self.assertRaisesRegex(dataclasses.FrozenInstanceError,
+                                "cannot assign to field"):
+      # noinspection PyDataclass
+      card.suit = Suit.HEARTS
+
+  def test_serialization(self):
+    from pickle import dumps, loads
+    for card_value in CardValue:
+      for suit in Suit:
+        card = Card(card_value, suit)
+        self.assertEquals(card, loads(dumps(card)))
