b3c636a0458defba7f4dbb3a36e4792b85ed7bb3
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-04-25 15:54:07 +0200

    Add the PlayerID enum.

---
+++ src/model/player_id.py
@@ -0,0 +1,16 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import enum
+
+
+@enum.unique
+class PlayerId(enum.Enum):
+  """Identifiers for the two players in a game of Schnapsen."""
+  ONE = enum.auto()
+  TWO = enum.auto()
+
+  def opponent(self) -> "PlayerId":
+    """Utility method that can be used to get the opponent of a given player."""
+    return PlayerId.TWO if self == PlayerId.ONE else PlayerId.ONE
---
+++ src/model/player_id_test.py
@@ -0,0 +1,22 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import unittest
+
+from model.player_id import PlayerId
+
+
+class PlayerIdTest(unittest.TestCase):
+    def test_creation_from_int(self):
+        """Make sure you cannot create invalid player IDs through int args."""
+        with self.assertRaisesRegex(ValueError, "0 is not a valid PlayerId"):
+            PlayerId(0)
+        PlayerId(1)
+        PlayerId(2)
+        with self.assertRaisesRegex(ValueError, "3 is not a valid PlayerId"):
+            PlayerId(3)
+
+    def test_opponent(self):
+        self.assertEqual(PlayerId.ONE, PlayerId.TWO.opponent())
+        self.assertEqual(PlayerId.TWO, PlayerId.ONE.opponent())
