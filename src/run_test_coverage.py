98a85e3968c9e70571977cd8cfb220a1ff8c4c99
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-04-27 21:13:33 +0200

    Exclude utility scripts from test coverage runs.

--- src/run_test_coverage.py
+++ src/run_test_coverage.py
@@ -14,16 +14,14 @@ def run_all_tests_with_coverage() -> float:
   Based on this: https://coverage.readthedocs.io/en/coverage-5.5/api.html#api
   :return The coverage percentage.
   """
-  current_dir = os.getcwd()
   tests_file_pattern = "*_test.py"
-  current_file = os.path.basename(__file__)
-  cov = coverage.Coverage(branch=True, source=[current_dir],
-                          omit=[tests_file_pattern, current_file])
+  cov = coverage.Coverage(branch=True, source=["./model"],
+                          omit=[tests_file_pattern])
   cov.start()
 
   # Discover and run all tests.
   loader = unittest.TestLoader()
-  tests = loader.discover(current_dir, pattern=tests_file_pattern)
+  tests = loader.discover(os.getcwd(), pattern=tests_file_pattern)
   test_runner = unittest.runner.TextTestRunner()
   test_runner.run(tests)
 
@@ -31,7 +29,7 @@ def run_all_tests_with_coverage() -> float:
   cov.save()
 
   # Generate the html report.
-  html_report_dir = os.path.join(current_dir, "htmlcov")
+  html_report_dir = "htmlcov"
   cov.html_report(directory=html_report_dir)
   html_path = "file:///%s/index.html" % "/".join(
     os.path.abspath(html_report_dir).split("\\"))
