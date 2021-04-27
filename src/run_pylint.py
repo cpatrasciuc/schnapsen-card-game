5a834f8f9308988ed84b456cb564cfa270817d64
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-04-27 21:02:40 +0200

    Add a virtual env and a script that runs pylint.

---
+++ src/run_pylint.py
@@ -0,0 +1,36 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import os
+
+import pylint.lint
+
+
+def get_all_python_files():
+  py_files = []
+  for root, _, files in os.walk("."):
+    for file in files:
+      if file.endswith(".py"):
+        py_files.append(os.path.join(root, file))
+  return py_files
+
+
+def run_pylint():
+  disabled_ckecks = [
+    "missing-module-docstring",
+    "missing-class-docstring",
+    "missing-function-docstring",
+    "fixme",
+    "locally-disabled"
+  ]
+  pylint_opts = [
+    "--indent-string='  '",
+    "-j 0",  # Run in parallel on all available processors
+    "--disable=" + ",".join(disabled_ckecks),
+  ]
+  pylint.lint.Run(pylint_opts + get_all_python_files())
+
+
+if __name__ == "__main__":
+  run_pylint()
