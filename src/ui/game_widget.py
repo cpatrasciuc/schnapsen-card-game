2a2e426f434a9afa7c3c0941b6889165e58de760
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-05-14 03:01:55 +0200

    Update cards based on window resize.

--- src/ui/game_widget.py
+++ src/ui/game_widget.py
@@ -42,6 +42,11 @@ class GameWidget(FloatLayout):
     self.ids.computer_cards_box.add_widget(cards_top)
     self._cards_top = cards_top
 
+    self._player_cards = []
+    self._computer_cards = []
+    self._talon_cards = []
+    self._trump_card = None
+
   def init_cards(self):
     # Instantiate the cards
     cards = Card.get_all_cards()
@@ -50,22 +55,17 @@ class GameWidget(FloatLayout):
     for i, card in enumerate(cards[:5]):
       card_widget = CardWidget(do_rotation=False, do_scale=False)
       card_widget.card_name = f"{card.card_value.name}\n{card.suit.name}"
-      card_widget.pos = self._cards_bot.get_relative_pos(0, i)
-      card_widget.size = self._cards_bot.get_card_size()
+      self._player_cards.append(card_widget)
       self.add_widget(card_widget)
 
     for i, card in enumerate(cards[5:10]):
       card_widget = CardWidget(do_rotation=False, do_scale=False,
                                do_translation=False)
       card_widget.card_name = f"{card.card_value.name}\n{card.suit.name}"
-      rel_x, rel_y = self._cards_top.get_relative_pos(0, i)
-      card_widget.pos = self._cards_top.x + rel_x, self._cards_top.y + rel_y
-      card_widget.size = self._cards_bot.get_card_size()
       card_widget.set_visible(False)
+      self._computer_cards.append(card_widget)
       self.add_widget(card_widget)
 
-    pos = self._talon.get_trump_card_pos()
-    size = self._talon.get_trump_card_size()
     card = cards[11]
     card_widget = CardWidget(do_rotation=False, do_scale=False,
                              do_translation=False)
@@ -74,20 +74,36 @@ class GameWidget(FloatLayout):
     m.rotate(3.14 / 2, 0, 0, 1)
     card_widget.apply_transform(m, anchor=(
       card_widget.width / 2, card_widget.height / 2))
-    card_widget.size = size[1], size[0]
-    card_widget.pos = pos
+    self._trump_card = card_widget
     self.add_widget(card_widget)
 
-    pos = self._talon.get_talon_pos()
-    size = self._talon.get_talon_size()
     for i, card in enumerate(cards[12:]):
       card_widget = CardWidget(do_rotation=False, do_scale=False,
                                do_translation=False)
       card_widget.card_name = f"{card.card_value.name}\n{card.suit.name}"
+
+      card_widget.set_visible(False)
+      self._talon_cards.append(card_widget)
+      self.add_widget(card_widget)
+
+  def _do_layout_cards(self):
+    for i, card_widget in enumerate(self._player_cards):
+      card_widget.pos = self._cards_bot.get_relative_pos(0, i)
+      card_widget.size = self._cards_bot.get_card_size()
+    for i, card_widget in enumerate(self._computer_cards):
+      rel_x, rel_y = self._cards_top.get_relative_pos(0, i)
+      card_widget.pos = self._cards_top.x + rel_x, self._cards_top.y + rel_y
+      card_widget.size = self._cards_bot.get_card_size()
+    if self._trump_card is not None:
+      size = self._talon.get_trump_card_size()
+      self._trump_card.size = size[1], size[0]
+      self._trump_card.pos = self._talon.get_trump_card_pos()
+
+    pos = self._talon.get_talon_pos()
+    size = self._talon.get_talon_size()
+    for i, card_widget in enumerate(self._talon_cards):
       card_widget.pos = pos[0] + i * 2, pos[1] - i * 2
       card_widget.size = size
-      card_widget.set_visible(False)
-      self.add_widget(card_widget)
 
   def do_layout(self, *args, **kwargs):
     self.ids.computer_tricks.height = 0.25 * self.height
@@ -104,6 +120,7 @@ class GameWidget(FloatLayout):
     width, _ = self.ids.computer_tricks.children[0].get_card_size()
     rel_x, _ = self.ids.computer_tricks.children[0].get_relative_pos(0, 7)
     self.ids.talon.width = rel_x + width
+    self._do_layout_cards()
     super().do_layout(*args, **kwargs)
 
   def trigger_layout(self):
