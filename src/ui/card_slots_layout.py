9a4dcc9da5eb8ed03caa4bb3ffdec167c448631a
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-05-15 15:15:37 +0200

    Add the CardSlotsLayout class.

---
+++ src/ui/card_slots_layout.py
@@ -0,0 +1,149 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+from typing import List, Optional, Tuple
+
+from kivy.base import runTouchApp
+from kivy.uix.layout import Layout
+from kivy.uix.widget import Widget
+
+from ui.debuggable_widget import DebuggableWidget
+
+
+class CardSlotsLayout(Layout, DebuggableWidget):
+  """
+  A layout that displays the children widgets (cards) on a grid and maximizes
+  the area used while keeping the aspect ratio of the children constant.
+  """
+
+  # pylint: disable=too-many-instance-attributes
+  def __init__(self, aspect_ratio: float = 24 / 37, rows: int = 1,
+               cols: int = 1, align_top: bool = False, spacing: float = 0.0,
+               **kwargs):
+    """
+    Instantiates a new CardSlotsLayout.
+    :param aspect_ratio: The aspect ratio that is enforced on the children (
+    aspect_ratio = width / height).
+    :param rows: Number of rows.
+    :param cols: Number of columns.
+    :param align_top: If this is True and the layout cannot use its entire
+    height to display the children, it will display them starting from the top
+    border and leaving the unused area at the bottom of the layout. If this is
+    False, the unused area will be at the top.
+    :param spacing: The spacing used between children, in percentages of
+    children size. A spacing of 0.5 means the children will leave half their
+    width/height between them. A spacing of -0.5 means the children will overlap
+    on half of their width/height.
+    :param kwargs: Parameters to forward to the base class' constructor.
+    """
+    # pylint: disable=too-many-arguments
+    super().__init__(**kwargs)
+    self._rows = rows
+    self._cols = cols
+    self._ratio = aspect_ratio
+    self._align_top = align_top
+    self._spacing = spacing
+    self._slots: List[List[Optional[Widget]]] = [[None] * cols for _ in
+                                                 range(rows)]
+    self._card_size = 0, 0
+
+    # Trigger a call to do_layout() whenever position, size or children change.
+    # pylint: disable=no-member
+    self.fbind('children', self._trigger_layout)
+    self.fbind('pos', self._trigger_layout)
+    self.fbind('pos_hint', self._trigger_layout)
+    self.fbind('size_hint', self._trigger_layout)
+    self.fbind('size', self._trigger_layout)
+    # pylint: enable=no-member
+
+  @property
+  def rows(self) -> int:
+    return self._rows
+
+  @property
+  def cols(self) -> int:
+    return self._cols
+
+  def add_card(self, widget: Widget, row: int, col: int) -> None:
+    """
+    Adds a widget to the slot specified by row and col. The slot must be empty
+    (i.e., not already occupied by another widget).
+    """
+    assert row < self._rows and col < self._cols, (row, col)
+    assert self._slots[row][col] is None, (self._slots[row][col], row, col)
+    self._slots[row][col] = widget
+    super().add_widget(widget)
+
+  def remove_card(self, row: int, col: int) -> Optional[Widget]:
+    """
+    Removes a widget from the slot specified by row and col. If the slot is not
+    empty, it returns the widget that got removed.
+    """
+    assert row < self._rows and col < self._cols, (row, col)
+    widget = self._slots[row][col]
+    if widget is not None:
+      super().remove_widget(widget)
+      self._slots[row][col] = None
+    return widget
+
+  def _update_card_size(self) -> None:
+    slot_height = self.height / ((1 + self._spacing) * (self._rows - 1) + 1)
+    width_if_height_is_fixed = slot_height * self._ratio
+    if (width_if_height_is_fixed * (1 + self._spacing) * (
+        self._cols - 1) + width_if_height_is_fixed) <= self.width:
+      width = width_if_height_is_fixed
+      height = slot_height
+    else:
+      width = self.width / ((1 + self._spacing) * (self._cols - 1) + 1)
+      height = width / self._ratio
+    self._card_size = int(width), int(height)
+    assert abs(self._card_size[0] / self.card_size[1] - self._ratio) < 1e10
+
+  @property
+  def card_size(self) -> Tuple[int, int]:
+    return self._card_size
+
+  def _get_local_card_pos(self, row, col):
+    assert 0 <= row < self._rows and 0 <= col < self._cols, (row, col)
+    width, height = self._card_size
+    bottom_margin = 0
+    if self._align_top:
+      bottom_margin = self.height - height * (1 + self._spacing) * (
+          self._rows - 1) - height
+    return int(col * width * (1 + self._spacing)), int(
+      bottom_margin + (self._rows - row - 1) * height * (
+          1 + self._spacing))
+
+  def get_card_pos(self, row, col):
+    """
+    Returns the position of the grid slot specified by row and col, in window
+    coordinates.
+    """
+    return self.to_window(*self._get_local_card_pos(row, col), False, True)
+
+  def do_layout(self, *_):
+    """
+    This function is called when a layout is called by a trigger. That means
+    whenever the position, the size or the children of this layout change.
+    """
+    self._update_card_size()
+    for row in range(self._rows):
+      for col in range(self._cols):
+        if self._slots[row][col] is None:
+          continue
+        self._slots[row][col].pos = self.get_card_pos(row, col)
+        self._slots[row][col].size = self._card_size
+
+
+if __name__ == "__main__":
+  ROWS = 2
+  COLS = 5
+  cards_layout = CardSlotsLayout(rows=ROWS, cols=COLS, spacing=-0.25,
+                                 align_top=True)
+  for _row in range(ROWS):
+    for _col in range(COLS):
+      label = DebuggableWidget()
+      label.debug_text = f"Slot{_row},{_col}"
+      cards_layout.add_card(label, _row, _col)
+  runTouchApp(cards_layout)
---
+++ src/ui/card_slots_layout_test.py
@@ -0,0 +1,158 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import unittest
+
+from kivy.tests.common import GraphicUnitTest
+from kivy.uix.widget import Widget
+
+from ui.card_slots_layout import CardSlotsLayout
+
+
+class CardSlotsLayoutTest(unittest.TestCase):
+  def compute_layout(self, cards_layout: CardSlotsLayout, expected_size=None,
+                     expected_pos=None):
+    cards_layout.do_layout()
+    self.assertEqual(expected_size, cards_layout.card_size)
+    pos = [[cards_layout.get_card_pos(row, col) for col in
+            range(cards_layout.cols)] for row in range(cards_layout.rows)]
+    self.assertEqual(expected_pos, pos)
+
+  def test_one_row(self):
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 3, rows=1, cols=3)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 100), [[(0, 0), (33, 0), (66, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=3, rows=1, cols=3)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 11), [[(0, 0), (33, 0), (66, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=3, rows=1, cols=3,
+                                   align_top=True)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 11), [[(0, 89), (33, 89), (66, 89)]])
+
+  def test_one_column(self):
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 4, rows=3, cols=1)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (8, 33), [[(0, 66)], [(0, 33)], [(0, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=4, rows=3, cols=1)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (100, 25),
+                        [[(0, 50)], [(0, 25)], [(0, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=4, rows=3, cols=1,
+                                   align_top=True)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (100, 25),
+                        [[(0, 75)], [(0, 50)], [(0, 25)]])
+
+  def test_2x3(self):
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 3, rows=2, cols=3)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (16, 50),
+                        [[(0, 50), (16, 50), (32, 50)],
+                         [(0, 0), (16, 0), (32, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=3, rows=2, cols=3)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 11),
+                        [[(0, 11), (33, 11), (66, 11)],
+                         [(0, 0), (33, 0), (66, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=3, rows=2, cols=3,
+                                   align_top=True)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 11),
+                        [[(0, 89), (33, 89), (66, 89)],
+                         [(0, 78), (33, 78), (66, 78)]])
+
+  def test_spacing(self):
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 2, rows=2, cols=2,
+                                   spacing=0.0)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (25, 50),
+                        [[(0, 50), (25, 50)], [(0, 0), (25, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 2, rows=2, cols=2,
+                                   spacing=0.1)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (23, 47),
+                        [[(0, 51), (25, 51)], [(0, 0), (25, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 2, rows=2, cols=2,
+                                   spacing=0.5)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (20, 40),
+                        [[(0, 60), (30, 60)], [(0, 0), (30, 0)]])
+
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 2, rows=2, cols=2,
+                                   spacing=-0.5)
+    cards_layout.size = 100, 100
+    self.compute_layout(cards_layout, (33, 66),
+                        [[(0, 33), (16, 33)], [(0, 0), (16, 0)]])
+
+  def test_remove_missing_card(self):
+    cards_layout = CardSlotsLayout(rows=1, cols=3)
+    widget = Widget()
+    cards_layout.add_card(widget, 0, 0)
+    self.assertIs(widget, cards_layout.remove_card(0, 0))
+    self.assertIsNone(cards_layout.remove_card(0, 0))
+
+  def test_cannot_add_outside_of_the_grid(self):
+    cards_layout = CardSlotsLayout(rows=2, cols=3)
+    with self.assertRaises(AssertionError):
+      cards_layout.add_card(Widget(), 10, 2)
+    with self.assertRaises(AssertionError):
+      cards_layout.add_card(Widget(), 0, 20)
+
+  def test_cannot_add_on_already_occupied_slot(self):
+    cards_layout = CardSlotsLayout(rows=2, cols=3)
+    cards_layout.add_card(Widget(), 0, 0)
+    with self.assertRaises(AssertionError):
+      cards_layout.add_card(Widget(), 0, 0)
+
+
+class CardSlotsLayoutGraphicTest(GraphicUnitTest):
+  def test_children_are_resized_and_repositioned(self):
+    cards_layout = CardSlotsLayout(aspect_ratio=1 / 3, rows=1, cols=3)
+    cards_layout.size = 100, 100
+
+    # Create a widget at random coordinates with a random size.
+    widget = Widget()
+    widget.size = 123, 456
+    widget.pos = 78, 90
+    self.assertEqual([123, 456], widget.size)
+    self.assertEqual([78, 90], widget.pos)
+
+    # Add the widget to the CardSlotsLayout and check that it got resized and
+    # correctly positioned.
+    cards_layout.add_card(widget, 0, 1)
+    self.advance_frames(1)
+    self.assertEqual([33, 0], widget.pos)
+    self.assertEqual([33, 100], widget.size)
+
+    # Resize the CardSlotsLayout and check that the widget got resized and
+    # repositioned accordingly.
+    cards_layout.size = 900, 900
+    self.advance_frames(1)
+    self.assertEqual([300, 0], widget.pos)
+    self.assertEqual([300, 900], widget.size)
+
+    # Move the CardSlotsLayout and check that the widget got resized and
+    # repositioned accordingly.
+    cards_layout.pos = 10, 20
+    self.advance_frames(1)
+    self.assertEqual([10, 20], cards_layout.pos)
+    self.assertEqual([310, 20], widget.pos)
+    self.assertEqual([300, 900], widget.size)
+
+    # Remove the widget from the CardSlotsLayout, move the CardSlotsLayout and
+    # check that the size and position of the widget are not affected.
+    self.assertIs(widget, cards_layout.remove_card(0, 1))
+    cards_layout.pos = 30, 40
+    self.advance_frames(1)
+    self.assertEqual([310, 20], widget.pos)
+    self.assertEqual([300, 900], widget.size)
