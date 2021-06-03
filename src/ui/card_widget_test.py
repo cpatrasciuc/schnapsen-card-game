73b078996be1fae3f0147ff36a67d90ec5226c3a
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-06-03 03:29:38 +0200

    Remove unused import.

--- src/ui/card_widget_test.py
+++ src/ui/card_widget_test.py
@@ -2,7 +2,6 @@
 #  Use of this source code is governed by a BSD-style license that can be
 #  found in the LICENSE file.
 
-import unittest
 from unittest.mock import Mock
 
 from kivy.tests.common import UnitTestTouch
