6265c6b4fbc12633f8a832debfb2a059f150bc05
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-06-12 15:04:24 +0200

    Add main.py to the project's root folder.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2021 Cristian Patrasciuc. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+import os
+import sys
+
+from kivy.logger import Logger
+
+if __name__ == "__main__":
+  root_dir = os.path.dirname(os.path.abspath(__file__))
+  Logger.info("Main: Adding root dir to python path: %s", root_dir)
+  sys.path.append(root_dir)
+
+  from ui.schnapsen_app import SchnapsenApp
+
+  SchnapsenApp().run()
