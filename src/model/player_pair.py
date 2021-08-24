c46a84f79e0185c4ffb2905ba7ab6b17869ede94
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-08-24 04:01:37 +0200

    Improve type hinting for PlayerPair.

--- src/model/player_pair.py
+++ src/model/player_pair.py
@@ -3,11 +3,12 @@
 #  found in the LICENSE file.
 
 import dataclasses
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, Optional
 
 from model.player_id import PlayerId
 
 _TypeName = TypeVar('_TypeName')
+
 
 # TODO(tests): Add tests for hashing.
 @dataclasses.dataclass(unsafe_hash=True)
@@ -21,14 +22,14 @@ class PlayerPair(Generic[_TypeName]):
     * the cards in hand: a list of cards for each player
     * a trick: one card for each player
   """
-  one: _TypeName = None
-  two: _TypeName = None
+  one: Optional[_TypeName] = None
+  two: Optional[_TypeName] = None
 
-  def __getitem__(self, key: PlayerId):
+  def __getitem__(self, key: PlayerId) -> Optional[_TypeName]:
     self._check_key_type(key)
     return self.one if key == PlayerId.ONE else self.two
 
-  def __setitem__(self, key: PlayerId, value: _TypeName):
+  def __setitem__(self, key: PlayerId, value: Optional[_TypeName]) -> None:
     self._check_key_type(key)
     if key == PlayerId.ONE:
       self.one = value
@@ -36,6 +37,6 @@ class PlayerPair(Generic[_TypeName]):
       self.two = value
 
   @staticmethod
-  def _check_key_type(key):
+  def _check_key_type(key) -> None:
     if not isinstance(key, PlayerId):
       raise TypeError("Keys must be of type PlayerId, not %s." % type(key))
