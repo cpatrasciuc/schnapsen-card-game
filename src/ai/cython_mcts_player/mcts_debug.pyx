665058ccbe0a201d259481f4eaaea4b6d8b3adc9
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2021-09-23 03:22:35 +0200

    Use the stored rewards to compute and plot CIs for the final action scores.

--- src/ai/cython_mcts_player/mcts_debug.pyx
+++ src/ai/cython_mcts_player/mcts_debug.pyx
@@ -4,8 +4,12 @@
 
 # distutils: language=c++
 
+from typing import List, Tuple
+
+import numpy as np
 import pandas
 from pandas import DataFrame
+from scipy.stats import bootstrap
 
 from libcpp.vector cimport vector
 
@@ -23,11 +27,74 @@ from ai.cython_mcts_player.player_action cimport ActionType
 from ai.cython_mcts_player.player_action cimport to_python_player_action
 from ai.mcts_player import generate_permutations
 from ai.mcts_player_options import MctsPlayerOptions
+from ai.merge_scoring_infos_func import ActionsWithScores, max_average_ucb
 from model.game_state import GameState as PyGameState
+from model.player_action import PlayerAction
 
 def _add_rank_column(dataframe: DataFrame) -> None:
   dataframe["rank"] = dataframe["score"].sort_values().rank(method="min",
                                                             ascending=False)
+
+cdef _score_ci(Node *node):
+  ci_low = node.ucb
+  ci_upp = node.ucb
+  if (not node.fully_simulated) and (node.rewards != NULL):
+    if node.rewards.size() >= 2:
+      ci = bootstrap((list(node.rewards[0]),), np.mean, confidence_level=0.95,
+                     method='percentile')
+      ci_low = ci.confidence_interval.low
+      ci_upp = ci.confidence_interval.high
+  if node.player != node.parent.player:
+    ci_low, ci_upp = -ci_upp, -ci_low
+  return ci_low, ci_upp
+
+cdef _build_scoring_info_with_debug(Node *root_node):
+  scoring_info = build_scoring_info(root_node)
+  for i in range(MAX_CHILDREN):
+    if root_node.actions[i].action_type == ActionType.NO_ACTION:
+      break
+    if root_node.children[i] == NULL:
+      continue
+    py_action = to_python_player_action(root_node.actions[i])
+    ci_low, ci_upp = _score_ci(root_node.children[i])
+    scoring_info[py_action].score_low = ci_low
+    scoring_info[py_action].score_upp = ci_upp
+  return scoring_info
+
+def _max_average_ucb_with_ci(
+    actions_with_scores_list: List[ActionsWithScores]) -> List[
+  Tuple[PlayerAction, float, float, float]]:
+  """
+  Same as max_average_ucb(), but tries to compute come CIs for the final score
+  of each action.
+  WARNING: This is likely not correct from a statistics point of view.
+  """
+  scores = {}
+  scores_low = {}
+  scores_upp = {}
+  for actions_with_scores in actions_with_scores_list:
+    for action, score in actions_with_scores.items():
+      scores[action] = scores.get(action, []) + [score.score]
+      if score.score_low is not None:
+        scores_low[action] = scores_low.get(action, []) + [score.score_low]
+      if score.score_upp is not None:
+        scores_upp[action] = scores_upp.get(action, []) + [score.score_upp]
+  actions_and_scores = []
+  for action, ucbs in scores.items():
+    score_low = scores_low[action]
+    score_upp = scores_upp[action]
+    upp = None
+    low = None
+    if len(score_low) > 2 and len(score_upp) > 2:
+      ci = bootstrap((score_low,), np.mean, method="percentile")
+      low = ci.confidence_interval.low
+      ci = bootstrap((score_upp,), np.mean, method="percentile")
+      upp = ci.confidence_interval.high
+    elif len(score_low) == 1 and len(score_upp) == 1:
+      low = score_low[0]
+      upp = score_upp[0]
+    actions_and_scores.append((action, sum(ucbs) / len(ucbs), low, upp))
+  return actions_and_scores
 
 cdef _get_children_data(Node *root_node):
   cdef int i
@@ -41,28 +108,39 @@ cdef _get_children_data(Node *root_node):
       continue
     py_action = to_python_player_action(root_node.actions[i])
     node = root_node.children[i]
+    ci_low, ci_upp = _score_ci(node)
     data.append((
       str(py_action),
       (node.q if node.player == root_node.player else -node.q),
       node.n,
       (node.ucb if node.player == root_node.player else -node.ucb),
-      node.exploration_score))
+      node.exploration_score,
+      ci_low,
+      ci_upp,
+    ))
 
-  dataframe = DataFrame(data, columns=["action", "q", "n", "score", "exp_comp"])
+  dataframe = DataFrame(data, columns=["action", "q", "n", "score", "exp_comp",
+                                       "score_low", "score_upp"])
   _add_rank_column(dataframe)
   return dataframe
 
 def run_mcts_and_collect_data(py_game_state: PyGameState,
-                              options: MctsPlayerOptions):
+                              options: MctsPlayerOptions,
+                              iterations_step: int = 1):
   cdef GameState game_state = from_python_game_state(py_game_state)
   cdef Node *root_node = init_node(&game_state, NULL)
-  iteration = 1
+  cdef int iteration = 1
+  cdef bint is_fully_simulated = False
+  cdef int max_iterations = options.max_iterations
   dataframes = []
-  max_iterations = options.max_iterations
   while True:
-    is_fully_simulated = run_one_iteration(root_node,
-                                           options.exploration_param,
-                                           options.select_best_child)
+    for _ in range(iterations_step):
+      is_fully_simulated = run_one_iteration(root_node,
+                                             options.exploration_param,
+                                             options.select_best_child)
+      iteration += 1
+      if is_fully_simulated:
+        break
     dataframe = _get_children_data(root_node)
     dataframe["iteration"] = iteration
     dataframes.append(dataframe)
@@ -70,12 +148,12 @@ def run_mcts_and_collect_data(py_game_state: PyGameState,
       break
     if max_iterations is not None and iteration >= max_iterations:
       break
-    iteration += 1
   delete_tree(root_node)
   return pandas.concat(dataframes, ignore_index=True)
 
 def run_mcts_player_step_by_step(py_game_view: PyGameState,
-                                 options: MctsPlayerOptions):
+                                 options: MctsPlayerOptions,
+                                 iterations_step: int):
   cdef GameState game_view = from_python_game_state(py_game_view)
   cdef GameState game_state
   cdef vector[Node *] root_nodes
@@ -86,29 +164,46 @@ def run_mcts_player_step_by_step(py_game_view: PyGameState,
   from_python_permutations(generate_permutations(py_game_view, options),
                            &permutations)
 
-  cdef int i
+  cdef int i, j
   for i in range(permutations.size()):
     game_state = game_view
     populate_game_view(&game_state, &permutations[i], opponent_id)
     root_nodes.push_back(init_node(&game_state, NULL))
 
-  iteration = 1
+  cdef int iteration = 1
+  cdef bint is_fully_simulated, permutation_is_fully_simulated
   dataframes = []
   max_iterations = options.max_iterations
   while True:
     is_fully_simulated = True
+    for _ in range(iterations_step):
+      is_fully_simulated = True
+      for i in range(root_nodes.size()):
+        permutation_is_fully_simulated = run_one_iteration(
+          root_nodes[i], options.exploration_param, options.select_best_child)
+        is_fully_simulated = \
+          is_fully_simulated and permutation_is_fully_simulated
+      iteration += 1
+      if is_fully_simulated:
+        break
 
     actions_with_scoring_infos = []
-    for i in range(root_nodes.size()):
-      is_fully_simulated = is_fully_simulated and run_one_iteration(
-        root_nodes[i], options.exploration_param, options.select_best_child)
-      actions_with_scoring_infos.append(build_scoring_info(root_nodes[i]))
-    actions_and_scores = options.merge_scoring_info_func(
-      actions_with_scoring_infos)
+    for j in range(root_nodes.size()):
+      actions_with_scoring_infos.append(
+        _build_scoring_info_with_debug(root_nodes[j]))
+    if options.merge_scoring_info_func == max_average_ucb:
+      actions_and_scores = _max_average_ucb_with_ci(actions_with_scoring_infos)
+      dataframe = DataFrame(
+        data=[(str(action), score, score_low, score_upp) for
+              action, score, score_low, score_upp in actions_and_scores],
+        columns=["action", "score", "score_low", "score_upp"])
+    else:
+      actions_and_scores = options.merge_scoring_info_func(
+        actions_with_scoring_infos)
+      dataframe = DataFrame(
+        data=[(str(action), score) for action, score in actions_and_scores],
+        columns=["action", "score"])
 
-    dataframe = DataFrame(
-      data=[(str(action), score) for action, score in actions_and_scores],
-      columns=["action", "score"])
     dataframe["iteration"] = iteration
     _add_rank_column(dataframe)
     dataframes.append(dataframe)
@@ -117,8 +212,6 @@ def run_mcts_player_step_by_step(py_game_view: PyGameState,
       break
     if max_iterations is not None and iteration >= max_iterations:
       break
-
-    iteration += 1
 
   for i in range(root_nodes.size()):
     delete_tree(root_nodes[i])
--- src/ai/eval/mcts_debug.py
+++ src/ai/eval/mcts_debug.py
@@ -2,8 +2,6 @@
 #  Use of this source code is governed by a BSD-style license that can be
 #  found in the LICENSE file.
 
-import math
-
 from matplotlib import pyplot as plt
 from pandas import DataFrame
 
@@ -16,13 +14,26 @@ from model.game_state import GameState
 _hlines_for_scores = [1, 0.66, 0.33, 0, -0.33, -0.66, -1]
 
 
-def _plot_data(dataframe: DataFrame, column: str, ax, hlines=None):
-  for action in sorted(dataframe.action.drop_duplicates()):
+def _plot_data(dataframe: DataFrame, column: str, ax, hlines=None,
+               confidence_columns=None):
+  colors = "bgrcmyk"
+  for i, action in enumerate(sorted(dataframe.action.drop_duplicates())):
     action_df = dataframe[dataframe.action.eq(action)].sort_values(
       "iteration")
-    ax.plot(action_df.iteration, action_df[column], label=action)
+    ax.plot(action_df.iteration, action_df[column], label=action,
+            color=colors[i])
+    if confidence_columns is not None and \
+        confidence_columns[0] in dataframe.columns and \
+        confidence_columns[1] in dataframe.columns:
+      low = action_df[confidence_columns[0]]
+      upp = action_df[confidence_columns[1]]
+      ax.fill_between(action_df.iteration, low, upp, alpha=0.1, color=colors[i])
   ax.set_title(column)
   ax.legend(loc=0)
+  min_value = dataframe[column].min()
+  max_value = dataframe[column].max()
+  ax.set_ylim(
+    (min_value - 0.1 * abs(min_value), max_value + 0.1 * abs(max_value)))
   if hlines is not None:
     ax.hlines(hlines, xmin=0, xmax=max(dataframe.iteration), color="k",
               alpha=0.25, linestyles="--")
@@ -30,7 +41,8 @@ def _plot_data(dataframe: DataFrame, column: str, ax, hlines=None):
 
 def mcts_debug(game_state: GameState, options: MctsPlayerOptions):
   print("Collecting data...")
-  dataframe = run_mcts_and_collect_data(game_state, options)
+  dataframe = run_mcts_and_collect_data(game_state, options,
+                                        iterations_step=100)
   csv_path = "mcts_debug.csv"
   dataframe.to_csv(csv_path, index=False)
 
@@ -41,34 +53,32 @@ def mcts_debug(game_state: GameState, options: MctsPlayerOptions):
   # noinspection PyTypeChecker
   _, axes = plt.subplots(rows, cols, figsize=(10 * cols, 5 * rows * cols),
                          squeeze=False, sharex=False, sharey=False)
-
   _plot_data(dataframe, "q", axes[0, 0], _hlines_for_scores)
   _plot_data(dataframe, "n", axes[1, 0])
-  # TODO(mcts): Add some confidence boundaries to this plot.
-  _plot_data(dataframe, "score", axes[2, 0], _hlines_for_scores)
+  _plot_data(dataframe, "score", axes[2, 0], _hlines_for_scores,
+             ("score_low", "score_upp"))
   _plot_data(dataframe, "exp_comp", axes[3, 0])
-
   plt.tight_layout()
   plt.savefig("mcts_debug.png")
 
 
 def mcts_player_debug(game_state: GameState, options: MctsPlayerOptions):
   print("Collecting data...")
-  dataframe = run_mcts_player_step_by_step(game_state, options)
+  dataframe = run_mcts_player_step_by_step(game_state, options,
+                                           iterations_step=500)
   csv_path = "mcts_player_debug.csv"
   dataframe.to_csv(csv_path, index=False)
 
   print("Plotting results...")
-  # TODO(mcts): Add some confidence boundaries to this plot.
-  _plot_data(dataframe, "score", plt.gca(), _hlines_for_scores)
+  _plot_data(dataframe, "score", plt.gca(), _hlines_for_scores,
+             confidence_columns=("score_low", "score_upp"))
   plt.tight_layout()
   plt.savefig("mcts_player_debug.png")
 
 
 if __name__ == "__main__":
   main_wrapper(
-    lambda: mcts_player_debug(GameState.new(random_seed=0).next_player_view(),
+    lambda: mcts_player_debug(GameState.new(random_seed=60).next_player_view(),
                               MctsPlayerOptions(max_iterations=4000,
-                                                max_permutations=200,
-                                                exploration_param=math.sqrt(2),
+                                                max_permutations=20,
                                                 select_best_child=True)))
